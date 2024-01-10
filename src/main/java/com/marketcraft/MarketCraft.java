d3e87d49bf424bff636ba468c9ce5880a91ceb49
Slimer <legodude063@gmail.com>
2024-01-09 20:10:26 -0500

    Initial commit

---
+++ src/main/java/com/marketcraft/MarketCraft.java
@@ -0,0 +1,17 @@
+package com.marketcraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MarketCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
